87bb0a653a4fd5572537631dce514b124fc9f629
K.G.Dulanka Chamathsra Yapa <nayanakumari417@gmail.com>
2025-05-07 14:40:00 +0530

    edit backend files to git

---
+++ backend/src/main/java/com/backend/backend/Model/CommentDto.java
@@ -0,0 +1,17 @@
+package com.backend.backend.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDto {
+
+    private String id;
+    private String comment;
+}
+
