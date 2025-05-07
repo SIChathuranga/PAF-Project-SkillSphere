45fafb42a058a495afba7ea8b6ec477bdecce3a8
S.I.Chathuranga <134764717+SIChathuranga@users.noreply.github.com>
2025-05-07 14:28:05 +0530

    delete

--- Frontend/frontend/src/utils/timeUtils.js
+++
@@ -1,6 +0,0 @@
-export const isExpired = (timestamp) => {
-    const twentyFourHours = 24 * 60 * 60 * 1000;
-    const now = new Date().getTime();
-    return now - new Date(timestamp).getTime() > twentyFourHours;
-  };
-  
