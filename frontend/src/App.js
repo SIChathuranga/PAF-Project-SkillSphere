03734e04fa61bcbe8e0264a52c81180dd622d05e
Savindu Nawarathne <savindunawarathne2000@gmail.com>
2025-05-10 01:29:18 +0530

    learning plan insider
    learning plan insider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import LearningPlanInsider from '../src/pages/LearningPlanInsider'
 function App() {
   return (
   <div className="App">
-    <h1>hello</h1>
+   <LearningPlanInsider/>
   </div>
   );
 }
