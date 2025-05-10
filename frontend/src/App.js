e828abac824c900a909a07c45d0297820035e5b9
Diniru Karunarathne <99533573+DiniruKarunarathne@users.noreply.github.com>
2025-05-10 11:40:51 +0530

    fix erros and add tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 // import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+export default function App() {
   return (
-  <div className="App">
-    <h1>hello</h1>
-  </div>
+    <Router>
+      <div className="w-full min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-export default App; 
